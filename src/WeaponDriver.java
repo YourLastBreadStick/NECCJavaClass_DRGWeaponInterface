5a446a3edfb34387002d1708767032f14ba4dd54
Alex <grimmalexander07@gmail.com>
2025-10-24 12:27:06 -0500

    Update WeaponDriver.java

--- src/WeaponDriver.java
+++ src/WeaponDriver.java
@@ -6,6 +6,7 @@ public class WeaponDriver {
         for (Weapon g : Guns){
             int targetHealth = 1000;
             System.out.println("--- " + g.name() + " ---");
+            System.out.println("Owned by: " + g.possession());
             System.out.println("Total damage the gun can deal before empty: " + g.totalDamage() + " damage");
             System.out.println("Time to kill the target with " + targetHealth + "hp: " + g.timeToKill(targetHealth) + " seconds");
             System.out.println("Time to fully empty the gun of ammo: " + g.timeToEmpty() + " seconds");
